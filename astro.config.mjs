7dcd8f1fb4060af1b03ffbb3a3475081630151e4
magiaslab <social@magiaslab.com>
2025-06-22 18:49:21 +0200

    footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import icons from 'astro-icons';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icons()],
   vite: {
     envPrefix: ['CLOUDINARY_']
   }
